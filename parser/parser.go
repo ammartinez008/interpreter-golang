8653de5c3101867b7832e95741600b42a1750c58
alex martinez <almmartinez08@gmail.com>
2017-08-24 23:36:05 -0400

    fix syntax error in New() for parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,12 +1,34 @@
 package parser
 
 import (
+	"github.com/monkey_lang/ast"
 	"github.com/monkey_lang/lexer"
 	"github.com/monkey_lang/token"
 )
 
 type Parser struct {
-	l         *lexer.Lexer
+	l *lexer.Lexer
+
 	curToken  token.Token
 	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	//Read first token
+	// set peek token to next token
+	curToken = l.NextToken
+	peekToken = l.NextToken
+	return p
+}
+
+func (p *Parser) nextToken() {
+	curToken = peekToken
+	peekToken = p.l.NextToken
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+
 }
